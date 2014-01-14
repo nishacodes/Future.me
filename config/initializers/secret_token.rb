3fd233a37c27edcc3cb434f1eda111ed3bd3e819
nishacodes <nishabatra27@gmail.com>
2014-01-14 12:36:50 -0500

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Futureme::Application.config.secret_token = '6362e0dbb595ac229f6f0b710f999e14d81f69477432c7eab4e7c232d8d652b229849a7f7c2b3c56a048a4bea9e06bbce4cfff1ddfa8a6f7bcebf2403e7db45e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Futureme::Application.config.session_store :cookie_store, key: '_futureme_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Futureme::Application.config.session_store :active_record_store
