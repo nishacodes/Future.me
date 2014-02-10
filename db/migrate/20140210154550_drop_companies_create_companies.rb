977a14c2754301d3bb93965f7c497e4080bb5256
nishacodes <nishabatra27@gmail.com>
2014-02-10 10:48:55 -0500

    drop and create table companies

---
+++ db/migrate/20140210154550_drop_companies_create_companies.rb
@@ -0,0 +1,13 @@
+class DropCompaniesCreateCompanies < ActiveRecord::Migration
+  def change
+    drop_table :companies
+    create_table :companies do |t|
+      t.string   :name
+      t.integer  :linkedin_id
+      t.string   :url
+      t.string   :address
+      t.string   :display
+      t.timestamps
+  end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140210151945) do
+ActiveRecord::Schema.define(:version => 20140210154550) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
-    t.integer  "linkedin_id", :limit => 255
+    t.integer  "linkedin_id"
     t.string   "url"
     t.string   "address"
     t.string   "display"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "company_departments", :id => false, :force => true do |t|
