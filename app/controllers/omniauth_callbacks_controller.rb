2c417be6ac49c7290c89e1e6366a12563c2213f5
nishacodes <nishabatra27@gmail.com>
2014-03-03 18:35:21 -0500

    comment out redirect after sign up w/linkedin

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 		user = User.from_omniauth(request.env["omniauth.auth"])
 		if user.persisted?
 			flash.notice = "Signed in w/ LinkedIn"
-			sign_in_and_redirect "#/view"
+			# sign_in_and_redirect "#/view"
 		else
 			session["devise.user_attributes"] = user.attributes
 			redirect_to new_user_registration_url # "#/view" 
