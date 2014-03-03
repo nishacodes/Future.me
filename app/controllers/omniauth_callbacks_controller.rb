1716a1387eb81d027ae4b7c4d8ed13086f6de2dc
nishacodes <nishabatra27@gmail.com>
2014-03-03 18:39:11 -0500

    revert to old version of omniauth controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -5,9 +5,10 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 		if user.persisted?
 			flash.notice = "Signed in w/ LinkedIn"
 			# sign_in_and_redirect "#/view"
+			sign_in_and_redirect user
 		else
 			session["devise.user_attributes"] = user.attributes
-			redirect_to new_user_registration_url # "#/view" 
+			# redirect_to new_user_registration_url # "#/view" 
 		end
 	end
 end
